37ea5a8db24920dea73f608058e48bca306e501d
Julian Haeger <julianhaeger@gmail.com>
2015-12-02 17:31:44 +0000

    Observable will follow a stream it is given at startup

--- lib/observable.ex
+++ lib/observable.ex
@@ -1,4 +1,11 @@
 defmodule StreamTools.Observable do
+  def start_link(stream) do
+    {:ok, event_manager} = GenEvent.start_link
+    result = {:ok, agent} = Agent.start_link(fn -> %{events: event_manager, value: :unset} end)
+    start_follower(stream, agent)
+    result
+  end
+
   def start_link do
     {:ok, event_manager} = GenEvent.start_link
     Agent.start_link(fn -> %{events: event_manager, value: :unset} end)
@@ -14,4 +21,9 @@ defmodule StreamTools.Observable do
   def stream_from_previous(observable), do: [value(observable)] |> Stream.concat(get_update_stream(observable))
 
   defp get_update_stream(observable), do: observable |> Agent.get(&Map.get(&1, :events)) |> GenEvent.stream
+
+  defp start_follower(stream, target) do
+    IO.puts("following #{inspect target}")
+    spawn_link(fn -> stream |> Stream.each(&set(target,&1)) |> Stream.run() end)
+  end
 end
--- test/observable_test.exs
+++ test/observable_test.exs
@@ -33,4 +33,24 @@ defmodule StreamTools.ObservableTests do
 
     assert_receive {:stream_values, [5, 6]}
   end
+
+  test "giving a stream on start causes observable to follow it" do
+    test_pid = self
+    {:ok, events} = GenEvent.start_link
+    {:ok, observable} = Observable.start_link GenEvent.stream(events)
+    :timer.sleep(50)
+
+    spawn_link fn ->
+      stream_values =
+        observable
+        |> Observable.stream_from_previous
+        |> Stream.take(2)
+        |> Enum.to_list
+
+      send test_pid, {:stream_values, stream_values}
+    end
+    :timer.sleep(50)
+    GenEvent.notify(events, 5)
+    assert_receive {:stream_values, [:unset, 5]}
+  end
 end
