e28efe50d50078bf51d327c334dad9627c5dc801
Julian Haeger <julianhaeger@gmail.com>
2015-12-16 08:11:27 +0000

    Adding fields to mix.exs for hex publishing

--- mix.exs
+++ mix.exs
@@ -1,10 +1,14 @@
-defmodule StreamTools.Mixfile do
+defmodule StreamWeaver.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :stream_tools,
+    [app: :stream_weaver,
      version: "0.0.1",
      elixir: "~> 1.0",
+     name: "StreamWeaver",
+     maintainers: ["julianhaeger@gmail.com"],
+     links: ["https://github.com/Foo42/elixir_stream_tools"],
+     description: "Library for working with streams",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
